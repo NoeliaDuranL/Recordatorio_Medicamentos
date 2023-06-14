1c919115dbf1785db96451e8d52db09b858223c5
Oscar Manuel Quevedo Oliva <102621659+manuQuevedO@users.noreply.github.com>
2023-06-14 09:44:16 -0400

    Se comentó el codigo de horariosActivity y MedicamenActivity

--- app/src/main/java/com/example/recordatoriomedicamentos/HorariosActivity.kt
+++ app/src/main/java/com/example/recordatoriomedicamentos/HorariosActivity.kt
@@ -12,48 +12,50 @@ import android.widget.ToggleButton
 import java.util.Calendar
 
 class HorariosActivity : AppCompatActivity() {
-    var alarmTimePicker : TimePicker? = null
-    var pendingIntent :PendingIntent? = null
-    var alarmManager :AlarmManager? = null
-
+    var alarmTimePicker: TimePicker? = null  // Selector de tiempo para la alarma
+    var pendingIntent: PendingIntent? = null  // Intent pendiente para la emisión de la alarma
+    var alarmManager: AlarmManager? = null  // Administrador de alarmas del sistema
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_horarios)
-        alarmTimePicker = findViewById(R.id.timePicker)
-        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+
+        alarmTimePicker = findViewById(R.id.timePicker)  // Obtener la referencia del selector de tiempo del diseño
+        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager  // Obtener una instancia del servicio de alarmas del sistema
     }
+
     fun onToggleClicked(view: View) {
-        var time : Long
-        if((view as ToggleButton).isChecked){
-            Toast.makeText(this,"Alarm ENCENDIDO",Toast.LENGTH_SHORT).show()
+        var time: Long  // Variable para almacenar el tiempo de la alarma
 
-            var calendar = Calendar.getInstance()
+        if ((view as ToggleButton).isChecked) {  // Verificar si el botón de alternancia está marcado (activado)
+            Toast.makeText(this, "Alarm ENCENDIDO", Toast.LENGTH_SHORT).show()  // Mostrar mensaje de alarma encendida
 
-            calendar[Calendar.HOUR_OF_DAY]= alarmTimePicker!!.currentHour
-            calendar[Calendar.MINUTE]= alarmTimePicker!!.currentMinute
+            var calendar = Calendar.getInstance()  // Crear una instancia de Calendar con la fecha y hora actuales
 
-            val intent = Intent(this,AlarmaReceiver::class.java)
+            calendar[Calendar.HOUR_OF_DAY] = alarmTimePicker!!.currentHour  // Establecer la hora del calendario con el valor seleccionado en el selector de tiempo
+            calendar[Calendar.MINUTE] = alarmTimePicker!!.currentMinute  // Establecer el minuto del calendario con el valor seleccionado en el selector de tiempo
+
+            val intent = Intent(this, AlarmaReceiver::class.java)  // Crear un intento para iniciar la clase AlarmaReceiver
 
             pendingIntent = PendingIntent.getBroadcast(
                 this,
                 0, intent, PendingIntent.FLAG_IMMUTABLE
-            )
-            time = calendar.timeInMillis - calendar.timeInMillis % 60000
-            if (System.currentTimeMillis()>time){
-                time = if (Calendar.AM_PM ==0){
-                    time +1000*60*60*12
-                }
-                else{
-                    time + 1000 * 60 *60*24
+            )  // Obtener un PendingIntent para la emisión de difusión utilizando el intento creado
+
+            time = calendar.timeInMillis - calendar.timeInMillis % 60000  // Calcular el tiempo en milisegundos al que se programará la alarma
+
+            if (System.currentTimeMillis() > time) {  // Verificar si el tiempo programado ya ha pasado
+                time = if (Calendar.AM_PM == 0) {  // Si es así, ajustar el tiempo programado para el próximo día
+                    time + 1000 * 60 * 60 * 12  // Sumar 12 horas si es AM
+                } else {
+                    time + 1000 * 60 * 60 * 24  // Sumar 24 horas si es PM
                 }
             }
-            alarmManager!!.setRepeating(AlarmManager.RTC_WAKEUP,time,1000,pendingIntent)
-        }
-        else{
-            alarmManager!!.cancel(pendingIntent)
-            Toast.makeText(this,"Alarm APAGADO",Toast.LENGTH_SHORT).show()
 
+            alarmManager!!.setRepeating(AlarmManager.RTC_WAKEUP, time, 1000, pendingIntent)  // Programar una alarma repetitiva utilizando el AlarmManager
+        } else {
+            alarmManager!!.cancel(pendingIntent)  // Cancelar la alarma programada anteriormente utilizando el mismo PendingIntent
+            Toast.makeText(this, "Alarm APAGADO", Toast.LENGTH_SHORT).show()  // Mostrar mensaje de alarma apagada
         }
     }
 }
--- app/src/main/java/com/example/recordatoriomedicamentos/MedicamentosActivity.kt
+++ app/src/main/java/com/example/recordatoriomedicamentos/MedicamentosActivity.kt
@@ -2,10 +2,35 @@ package com.example.recordatoriomedicamentos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MedicamentosActivity : AppCompatActivity() {
+    private lateinit var editTextMedicinaNombre: EditText  // Campo de texto para ingresar el nombre del medicamento
+    private lateinit var editTextCantidad: EditText  // Campo de texto para ingresar la cantidad del medicamento
+    private lateinit var buttonGuardar: Button  // Botón para guardar el medicamento
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_medicamentos)
+
+        editTextMedicinaNombre = findViewById(R.id.editTextMedicinaNombre)  // Obtener referencia del campo de texto del nombre del medicamento
+        editTextCantidad = findViewById(R.id.editTextCantidad)  // Obtener referencia del campo de texto de la cantidad del medicamento
+        buttonGuardar = findViewById(R.id.buttonGuardar)  // Obtener referencia del botón de guardar
+
+        buttonGuardar.setOnClickListener {
+            val medicinaNombre = editTextMedicinaNombre.text.toString()  // Obtener el texto ingresado en el campo de texto del nombre del medicamento
+            val cantidad = editTextCantidad.text.toString()  // Obtener el texto ingresado en el campo de texto de la cantidad del medicamento
+
+            guardarMedicamento(medicinaNombre, cantidad)  // Llamar a la función para guardar el medicamento
+        }
+    }
+
+    private fun guardarMedicamento(medicinaNombre: String, cantidad: String) {
+        // Aquí puedes implementar la lógica para guardar los datos del medicamento en la base de datos o realizar cualquier otra acción requerida.
+
+        // Ejemplo: Mostrar un mensaje de éxito después de guardar los datos.
+        Toast.makeText(this, "Medicamento guardado: $medicinaNombre, Cantidad: $cantidad", Toast.LENGTH_SHORT).show()
     }
 }
